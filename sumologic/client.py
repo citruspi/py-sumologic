6d548bc80c029232d20c47afd011cdaad0d95f48
Mihir Singh <git.service@mihirsingh.com>
2015-09-21 21:53:33 -0400

    Added support for retrieving auth credentials from the environment

--- sumologic/client.py
+++ sumologic/client.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 import requests
+
+import sumologic.exceptions
 
 
 class Client(object):
@@ -15,9 +19,32 @@ class Client(object):
         :param access_id: The Access ID to connect with
         :param access_key: The Access Key to connect with
         """
-        self.auth=(access_id, access_key)
+        self.load_authentication(access_id, access_key)
 
         self.session = requests.Session()
 
         self.session.auth = self.auth
         self.session.headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+    def load_authentication(self, access_id=None, access_key=None):
+        """
+        Load the Access ID and Access Key to be used for authentication.
+        The arguments will be checked first, followed by environment
+        variables.
+
+        :param access_id: The Access ID to connect with.
+        :type access_id: basestring
+        :param access_key: The Access Key to connect with.
+        :type access_key: basestring
+        :raises sumologic.exceptions.AuthenticationError: When it fails
+            to load authentication information
+        :rtype: None
+        """
+        if access_id is None:
+            access_id = os.environ.get('SUMOLOGIC_ACCESS_ID', None)
+
+        if access_key is None:
+            access_key = os.environ.get('SUMOLOGIC_ACCESS_KEY', None)
+
+        if access_id is None or access_key is None:
+            raise sumologic.exceptions.AuthenticationError()
